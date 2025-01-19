d2871e760ab8d480d17721d7cf515c3a2368ae11
Takahiro Itou <6907757+takahiro-itou@users.noreply.github.com>
2025-01-19 19:53:29 +0900

    実行環境の構築手順
    (1) python3 -m venv で環境を作成

--- Install.sh
+++ Install.sh
@@ -1,1 +1,7 @@
 #!/usr/bin/bash -xue
+
+current_srcfile="${BASH_SOURCE:-$0}"
+script_dir=$(readlink -f "$(dirname "${current_srcfile}")")
+
+/usr/bin/python3 --version
+/usr/bin/python3 -m venv .env
